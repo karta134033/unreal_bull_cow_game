315842e9aac48ebc58d0d6243a62d788f33d6863
karta134033 <karta134033@gmail.com>
2020-07-14 21:24:36 +0800

    2020/07/14 新增:1. EndGame() 功能

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,33 +4,45 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("Welcome to Bull Cows"));
     PrintLine(TEXT("Press Tab and Enter to continue..."));
-
     InitGame();
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
-    if (Lives > 0) {
-        if (Input == HiddenWord) {
-            PrintLine(TEXT("Win"));
-        } else {
-            PrintLine(TEXT("Fuck off"));
-            Lives--;
+    if (game_over) {
+        ClearScreen();
+        InitGame();
+    } else {
+        if (Lives > 0) {
+            if (Input == HiddenWord) {
+                PrintLine(TEXT("You Win"));
+                EndGame();
+            } else {
+                --Lives;
+                PrintLine(TEXT("Fuck off"));
+                PrintLine(TEXT("%d lives left"), Lives);
+            }
         }
+        if (Lives == 0) {
+            PrintLine(TEXT("Game Over"));
+            EndGame();
+        }
+        // PrintLine(TEXT("HiddenWord is %s"), *HiddenWord); // PrintLine 需要運用指標才能找到string的array  i.g.cake -> [c, a, k, e, \0]
+        PrintLine(Input);
+
     }
-    if (Lives == 0) {
-        PrintLine(TEXT("You've done."));
-    }
-    PrintLine(TEXT("%d lives left"), Lives);
-    PrintLine(TEXT("HiddenWord is %i long"), HiddenWord.Len());
-    PrintLine(TEXT("HiddenWord is %s"), *HiddenWord); // PrintLine 需要運用指標才能找到string的array  i.g.cake -> [c, a, k, e, \0]
-    PrintLine(Input);
 }
 
 void UBullCowCartridge::InitGame() {
     HiddenWord = TEXT("cake");
-    Lives = 5;
+    Lives = HiddenWord.Len();
+    game_over = false;
+    PrintLine(TEXT("Welcome to Bull Cows"));
+    PrintLine(TEXT("The hidden word is %i long"), HiddenWord.Len());
+}
+
+void UBullCowCartridge::EndGame() {
+    game_over = true;
+    PrintLine(TEXT("Press enter to play again."));
 }
